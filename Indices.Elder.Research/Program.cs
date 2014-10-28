84cd98fe24952e37394f861571dfc929ab2d3ada
Samuel Pets <samuel.pets@neuralsoft.com.ar>
2014-10-28 10:48:28 -0300

    no message

---
+++ Indices.Elder.Research/Program.cs
@@ -0,0 +1,20 @@
+﻿using FinancialData.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YahooStockQuote.FinancialDataProvider;
+
+namespace Indices.Elder.Research {
+  class Program {
+    static void Main(string[] args) {
+      var symbol = YSQSymbol.YSQIndex.SNP;
+      var begin = new DateTime(2004, 1, 1);
+      var end = new DateTime(2008, 1, 1);
+      var samplePackage = new YSQProvider().GetHistory(new Asset(symbol, AssetType.Index), begin, end, null);
+      var barPackage = (IBarPackage)samplePackage;
+      var bars = barPackage.Samples;
+    }
+  }
+}
