371573637d8975e06b2b3106a1b40fc14d1876cb
Samuel Pets <samuel.pets@neuralsoft.com.ar>
2014-10-30 20:45:20 -0300

    no message

--- Charts/Series.cs
+++ Charts/Series.cs
@@ -103,49 +103,72 @@ namespace Charts {
       return grid;
     }
     public static FrameworkElement GetRenderingColumns(Series series, double actualWidth, double actualHeight, DateTime begin, DateTime end, double min, double max) {
-      if (min > 0 || max < 0) {
-        throw new InvalidDataException("GetRenderingColumns: min must be smaller than zero and max must be greater than zero.");
-      }
+      Console.WriteLine("actualWidth {0}, actualHeight {1}, begin {2}, end {3}, min {4}, max {5}", 
+        actualWidth, actualHeight, begin, end, min, max);
+      //if (min > 0 || max < 0) {
+      //  throw new InvalidDataException("GetRenderingColumns: min must be smaller than zero and max must be greater than zero.");
+      //}
       var samples = series.Drawables.Cast<Sample>().ToList();
       Grid grid = new Grid();
       double deltatimePeriod = (end - begin).TotalMilliseconds;
       double deltaMaxMin = max - min;
-      samples.ForEach(sample => {
-        if (sample.Instant >= begin && sample.Instant <= end && sample.Value != 0) {
-          Rectangle rectangle = new Rectangle();
-          rectangle.Width = 10; // calculate
-          rectangle.Fill = rectangle.Stroke = sample.Value > 0 ? Brushes.Green : Brushes.Red;
-          rectangle.ToolTip = string.Format("{0}: {1} {2:F4}", series.Name, sample.Instant, sample.Value);
-          //position computation
-          double deltatime = (sample.Instant - begin).TotalMilliseconds;
-          double xPosition = deltatime * actualWidth / deltatimePeriod;
-          double deltaValue = sample.Value - min;
-          double zeroPosition = -min * actualHeight / deltaMaxMin;
-          double YPosition = deltaValue * actualHeight / deltaMaxMin;
-          YPosition = actualHeight - YPosition; // inversion
-          //position setting
-          rectangle.HorizontalAlignment = HorizontalAlignment.Left;
-          rectangle.VerticalAlignment = VerticalAlignment.Top;
-          rectangle.Margin = new Thickness(xPosition - 1, zeroPosition < YPosition ? zeroPosition : YPosition, 0, 0);
-          //size adjustment
-          //TODO: generate negative columns
-          double rectangleHeight = Math.Abs(zeroPosition - YPosition);
-          rectangle.Height = rectangleHeight;
-          if (rectangle.Height == 0) rectangle.Height = 0.5;
-          //label
-          grid.Children.Add(new TextBlock() {
-            Text = string.Format(@"{0:F1}", sample.Value * 100.0),
-            HorizontalAlignment = HorizontalAlignment.Left,
-            VerticalAlignment = VerticalAlignment.Top,
-            Margin = new Thickness(
-              xPosition - (sample.Value > 0 ? 1 : 3),
-              YPosition + (sample.Value > 0 ? -12 : 0),
-              0, 0),
-            FontFamily = new FontFamily("Calibri"),
-            FontSize = 8,
-          });
-          grid.Children.Add(rectangle);
+      var relevantSamples = samples.Where(sample => sample.Instant >= begin && sample.Instant <= end && sample.Value != 0).ToList();
+      
+      double reclangleWidth = 10;// actualWidth / relevantSamples.Count;
+      if (relevantSamples.Count > 1) {
+        TimeSpan implicitInterval = relevantSamples[1].Instant - relevantSamples[0].Instant;
+        reclangleWidth = implicitInterval.TotalMilliseconds * actualWidth / deltatimePeriod;
+      }
+      
+      relevantSamples.ForEach(sample => {
+        Rectangle rectangle = new Rectangle();
+        rectangle.HorizontalAlignment = HorizontalAlignment.Left;
+        rectangle.VerticalAlignment = VerticalAlignment.Top;
+        rectangle.Width = reclangleWidth;
+        
+        rectangle.Fill = rectangle.Stroke = sample.Value > 0 ? Brushes.Green : Brushes.Red;
+        //rectangle.ToolTip = string.Format("{0}: {1} {2:F4}", series.Name, sample.Instant, sample.Value);
+
+        double deltatime = (sample.Instant - begin).TotalMilliseconds;
+        double xPosition = deltatime * actualWidth / deltatimePeriod;
+        //---
+        double deltaValue = sample.Value - min;
+        double deltaZero = 0 - min;
+        double zeroPosition = deltaZero * actualHeight / deltaMaxMin;
+        double yPosition;
+        if (sample.Value > 0) {
+          yPosition = actualHeight - (deltaValue * actualHeight / deltaMaxMin);
         }
+        else {
+          yPosition = actualHeight - zeroPosition;
+        }
+        rectangle.Height = Math.Abs(sample.Value) * actualHeight / deltaMaxMin;
+        if (rectangle.Height < 0.5) rectangle.Height = 0.5;
+        
+        //---
+        rectangle.Margin = new Thickness(xPosition, yPosition, 0, 0);
+
+        rectangle.ToolTip = string.Format("xPosition {0:F1}, yPosition {1:F1}, Width {2:F1}, Height {3:F1}, zeroPosition {4:F1}", xPosition, yPosition, rectangle.Width, rectangle.Height, zeroPosition);
+
+        /*
+        //var duration = sample.End - sample.Begin;
+        
+        //position computation
+        double xPosition = deltatime * actualWidth / deltatimePeriod;
+        double deltaValue = sample.Value - min;
+        double zeroPosition = -min * actualHeight / deltaMaxMin;
+        //double YPosition = deltaValue * actualHeight / deltaMaxMin;
+        //YPosition = actualHeight - YPosition; // inversion
+        //position setting
+        
+        double yPosition = zeroPosition;// < YPosition ? zeroPosition : YPosition;
+        
+        //size adjustment
+        double rectangleHeight = 0;// Math.Abs(zeroPosition - YPosition);
+        rectangle.Height = rectangleHeight;
+        if (rectangle.Height == 0) rectangle.Height = 0.5;
+        */
+        grid.Children.Add(rectangle);
       });
       return grid;
     }
--- Indices.Elder.Research/Program.cs
+++ Indices.Elder.Research/Program.cs
@@ -24,25 +24,32 @@ using YahooStockQuote.FinancialDataProvider;
 using TaLib.Extension;
 using Charts;
 using System.Windows.Media;
+using FinancialData;
 
 namespace Indices.Elder.Research {
   class Program {
     static void Main(string[] args) {
       string symbol = YSQSymbol.YSQIndex.SNP;
-      DateTime begin = new DateTime(2004, 1, 1);
-      DateTime end = new DateTime(2008, 1, 1);
+      DateTime begin = new DateTime(2003, 12, 1);
+      DateTime end = new DateTime(2004, 2, 1);
       ISamplePackage samplePackage = new YSQProvider().GetHistory(new Asset(symbol, AssetType.Index), begin, end, null);
       IBarPackage barPackage = (IBarPackage)samplePackage;
       List<IBar> bars = barPackage.Samples;
       TaResult ema13 = bars.EMA(13);
       BullPower bullPower = new BullPower(bars, 13);
 
+      //var sampleValues = new List<IInstantValue<double>>();
+      //sampleValues.Add(new InstantValue<double>(new DateTime(2004, 1, 1), 1.0));
+      //sampleValues.Add(new InstantValue<double>(new DateTime(2004, 1, 2), -1.0));
+      //sampleValues.Add(new InstantValue<double>(new DateTime(2004, 1, 3), 1.0));
+      //sampleValues.Add(new InstantValue<double>(new DateTime(2004, 1, 4), -1.0));
+
       ChartPool.CreateChart();
       ChartPool.ClearSeries();
       ChartPool.AddSeries(
         new List<Series> {
-          //new Series("Prices", ChartType.Lines, Colors.Black, bars.Select(x => new Sample(x.DateTime, (double)x.Close))),
-          //new Series("EMA(13)", ChartType.Lines, Colors.Red, ema13.InstantValues.Select(x => new Sample(x.DateTime, x.Value))),
+          new Series("Prices", ChartType.Lines, Colors.Black, bars.Select(x => new Sample(x.DateTime, (double)x.Close))),
+          new Series("EMA(13)", ChartType.Lines, Colors.Red, ema13.InstantValues.Select(x => new Sample(x.DateTime, x.Value))),
           new Series("BullPower(13)", ChartType.Columns, Colors.Blue, bullPower.InstantValues.Select(x => new Sample(x.DateTime, x.Value))),
         }
       );
